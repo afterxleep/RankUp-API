965445cb94be3983673ecc6c39f31426d8046c63
Daniel Bernal <daniel@banshai.com>
2019-07-22 23:17:19 -0500

    Configuration Environments

--- api/controllers/me/index.js
+++ api/controllers/me/index.js
@@ -1,35 +1,17 @@
-const moment = require('moment')
-const updateInterval = 86400000 // 1 day
-
 module.exports = {
 
-  friendlyName: "Me",
+  friendlyName: "Sync Me",
   description: "Get details of the currently logged in user",
 
   fn: async function() {
 
     var user = await User.find({
-      msid: this.req.user.msid
-    })
-
-    // If the user does not exist in the local store yet create it
+        msid: this.req.user.msid
+      })
+      .populate("location")
+      .populate("area")
     if (!user[0]) {
-      user = await User.create(this.req.user).fetch()
-    } else {
-      // Update the local user data once every 24h
-      let refreshDate = new Date().getTime()
-      if (user.updatedAt <= refreshDate - updateInterval) {
-        user = await User.update({
-            msid: this.req.user.msid
-          })
-          .set({
-            name: this.req.user.name,
-            email: this.req.user.email,
-            jobTitle: this.req.user.jobTitle,
-            image: this.req.user.image,
-          })
-          .fetch()
-      }
+      return this.res.status(404).send("Provided User does not exist in the local database yet.  Please register.");
     }
     return user[0]
 
